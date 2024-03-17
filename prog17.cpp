a5d7d72ac0e54d97ba7663aee5472fdf9d49d877
ARIN VASHISHTHA <121510816+arin2107@users.noreply.github.com>
2024-03-17 11:06:51 +0530

    Update prog17.cpp

--- prog17.cpp
+++ prog17.cpp
@@ -1,84 +1,70 @@
-//prim
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <limits>
-
+//prim 
+#include<bits/stdc++.h>
 using namespace std;
 
-const int INF = numeric_limits<int>::max();
+#define V 6		//No of vertices
 
-struct Edge {
-    int to;
-    int weight;
-    Edge(int t, int w) : to(t), weight(w) {}
-};
-
-
-struct Compare {
-    bool operator()(const pair<int, int>& a, const pair<int, int>& b) const {
-        return a.second > b.second;
-    }
-};
-
-// Prim's algorithm
-vector<Edge> prim(vector<vector<Edge>>& graph, int source, int& total_weight) {
-    int n = graph.size();
-    vector<Edge> mst; 
-    vector<bool> visited(n, false);
-    priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> pq;
-
-    visited[source] = true;
-    for (const Edge& e : graph[source])
-        pq.push({e.to, e.weight});
-
-    
-    while (!pq.empty()) {
-        int u = pq.top().first;
-        int w = pq.top().second;
-        pq.pop();
-
-        if (visited[u])
-            continue;
-
-        visited[u] = true;
-        mst.push_back(Edge(u, w));
-        total_weight += w;
-
-        for (const Edge& e : graph[u]) {
-            if (!visited[e.to])
-                pq.push({e.to, e.weight});
-        }
-    }
-
-    return mst;
+int selectMinVertex(vector<int>& value,vector<bool>& setMST)
+{
+	int minimum = INT_MAX;
+	int vertex;
+	for(int i=0;i<V;++i)
+	{
+		if(setMST[i]==false && value[i]<minimum)
+		{
+			vertex = i;
+			minimum = value[i];
+		}
+	}
+	return vertex;
 }
 
-int main() {
-    vector<vector<Edge>> graph(6);
+void findMST(int graph[V][V])
+{
+	int parent[V];		//Stores MST
+	vector<int> value(V,INT_MAX);	//Used for edge relaxation
+	vector<bool> setMST(V,false);	//TRUE->Vertex is included in MST
 
-    // Adding edges to the graph
-    graph[0].push_back(Edge(1, 2));
-    graph[0].push_back(Edge(2, 5));
-    graph[1].push_back(Edge(2, 1));
-    graph[1].push_back(Edge(3, 7));
-    graph[1].push_back(Edge(4, 6));
-    graph[2].push_back(Edge(4, 3));
-    graph[3].push_back(Edge(5, 9));
-    graph[4].push_back(Edge(5, 4));
+	//Assuming start point as Node-0
+	parent[0] = -1;	//Start node has no parent
+	value[0] = 0;	//start node has value=0 to get picked 1st
 
-    int source = 0; 
-    int total_weight = 0;
+	//Form MST with (V-1) edges
+	for(int i=0;i<V-1;++i)
+	{
+		//Select best Vertex by applying greedy method
+		int U = selectMinVertex(value,setMST);
+		setMST[U] = true;	//Include new Vertex in MST
 
-    
-    vector<Edge> minimum_spanning_tree = prim(graph, source, total_weight);
+		//Relax adjacent vertices (not yet included in MST)
+		for(int j=0;j<V;++j)
+		{
+			/* 3 constraints to relax:-
+			      1.Edge is present from U to j.
+			      2.Vertex j is not included in MST
+			      3.Edge weight is smaller than current edge weight
+			*/
+			if(graph[U][j]!=0 && setMST[j]==false && graph[U][j]<value[j])
+			{
+				value[j] = graph[U][j];
+				parent[j] = U;
+			}
+		}
+	}
+	//Print MST
+	for(int i=1;i<V;++i)
+		cout<<"U->V: "<<parent[i]<<"->"<<i<<"  wt = "<<graph[parent[i]][i]<<"\n";
+}
 
-    cout << "Edges of the minimum spanning tree:\n";
-    for (const Edge& e : minimum_spanning_tree)
-        cout << source << " - " << e.to << " (weight " << e.weight << ")\n";
+int main()
+{
+	int graph[V][V] = { {0, 4, 6, 0, 0, 0},
+						{4, 0, 6, 3, 4, 0},
+						{6, 6, 0, 1, 8, 0},
+						{0, 3, 1, 0, 2, 3},
+						{0, 4, 8, 2, 0, 7},
+						{0, 0, 0, 3, 7, 0} };
 
- 
-    cout << "Total weight of the minimum spanning tree: " << total_weight << endl;
-
-    return 0;
+	findMST(graph);	
+	return 0;
 }
